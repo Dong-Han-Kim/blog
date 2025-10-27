ae6eb8e0e82863ef1abf16f137be1294be33e10d
Dong Han <148463611+Dong-Han-Kim@users.noreply.github.com>
2025-10-27 22:45:17 +0900

    Feat/category page 구현 (#4)
    * fix: 다크 모드 개선
    
    * Feat: PC 및 Mobile 메뉴 구현
    
    * Feat: Category 페이지 구현

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -1,5 +1,47 @@
-function CategoryPage() {
-  return <div>Category Page</div>;
+import Card from '@/components/shared/Card';
+import { getPostsByCategory } from '@/lib/posts';
+import { PostMeta } from '@/types/common';
+
+interface CategoryPageProps {
+  params: { category: string };
+}
+
+function CategoryPage({ params }: CategoryPageProps) {
+  const { category } = params;
+  const posts: PostMeta[] | null = getPostsByCategory(category);
+
+  const sortedPosts =
+    posts &&
+    posts.sort((a, b) => {
+      if (!a.date || !b.date) return 0;
+      const firstDate = new Date(a.date).getTime();
+      const secondDate = new Date(b.date).getTime();
+      return secondDate - firstDate;
+    });
+
+  return (
+    <div className="w-full px-10 md:px-20 lg:px-50">
+      <h1 className="text-5xl mb-10">{category}</h1>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+        {sortedPosts &&
+          sortedPosts.map((post) => {
+            if (post.draft) return null;
+            return (
+              <Card
+                key={post.slug}
+                slug={post.slug}
+                category={post.category}
+                title={post.title}
+                description={post.description}
+                date={post.date}
+                thumbnail={post.thumbnail}
+                keywords={post.keywords}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default CategoryPage;
