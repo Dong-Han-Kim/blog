1e5a7462a03267c2048f8bdb02d79f05d22a11ea
Dong Han <148463611+Dong-Han-Kim@users.noreply.github.com>
2025-10-28 22:11:34 +0900

    재배포 (#8)
    * Chore: 초기 세팅
    
    * Chore: ESLint 설정
    
    * Feat: Home 기본 UI 구현 (#1)
    
    * Chore: 기초 폴더 구조 작성
    
    * Feat: home page에 데이터 렌더링 구현
    
    * Feat: 포스팅 글 기본 이미지 세팅
    
    * Feat: home ui 기초 구현
    
    * Feat: dark mode 기능 구현 (#2)
    
    * Feat/navigation (#3)
    
    * fix: 다크 모드 개선
    
    * Feat: PC 및 Mobile 메뉴 구현
    
    * Feat/category page 구현 (#4)
    
    * fix: 다크 모드 개선
    
    * Feat: PC 및 Mobile 메뉴 구현
    
    * Feat: Category 페이지 구현
    
    * Feat/Post 상세 페이지 구현 (#5)
    
    * fix: 다크 모드 개선
    
    * Feat: PC 및 Mobile 메뉴 구현
    
    * Feat: Category 페이지 구현
    
    * Feat: Post 상세페이지 구현
    
    * Fix/ 누락된 경로 예외 처리 추가 (#7)
    
    * fix: 다크 모드 개선
    
    * Feat: PC 및 Mobile 메뉴 구현
    
    * Feat: Category 페이지 구현
    
    * Feat: 경로 예외 처리 추가
    
    * Feat: console.log 제거

---
+++ src/constants/thumbnail.ts
@@ -0,0 +1,9 @@
+import { DefaultImage } from '../types/common';
+
+export const DEFAULT_THUMBNAIL: DefaultImage = {
+  HTML: '/thumbnail/html.jpg',
+  CSS: '/thumbnail/CSS.jpg',
+  JavaScript: '/thumbnail/javaScript.jpg',
+  Reactjs: '/thumbnail/react.jpg',
+  etc: '/thumbnail/etc.jpg',
+};
