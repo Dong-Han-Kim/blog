a82b764c6515179d9c185ad8cf00f850340ba1d0
Dong Han <148463611+Dong-Han-Kim@users.noreply.github.com>
2025-10-30 21:45:00 +0900

    Fix/Nav 및 컨텐츠 스타일 개선 (#11)
    * WIP: save work
    
    * Fix: folder rename
    
    * Fix: Nav 및 컨텐츠 스타일 개선

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -4,8 +4,8 @@ export const Default_Nav_items = [
   { name: 'CSS', path: '/blog/CSS', state: true },
   { name: 'JavaScript', path: '/blog/JavaScript', state: true },
   { name: 'TypeScript', path: '/blog/TypeScript', state: true },
-  { name: 'React.js', path: '/blog/Reactjs', state: true },
-  { name: 'Next.js', path: '/blog/Nextjs', state: true },
+  { name: 'React.js', path: '/blog/React', state: true },
+  { name: 'Next.js', path: '/blog/Next', state: true },
   { name: '상태관리', path: '/blog/state-management', state: false },
   { name: 'Projects', path: '/blog/projects', state: true },
 ];
