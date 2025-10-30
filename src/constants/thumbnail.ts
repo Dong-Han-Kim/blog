2c4f139779668db163c481f87cfde4876a578461
Dong-Han-Kim <dh32110@gmail.com>
2025-10-30 22:02:11 +0900

    Fix: 썸네일 경로 변경

--- src/constants/thumbnail.ts
+++ src/constants/thumbnail.ts
@@ -4,6 +4,6 @@ export const DEFAULT_THUMBNAIL: DefaultImage = {
   HTML: '/thumbnail/html.jpg',
   CSS: '/thumbnail/css.jpg',
   JavaScript: '/thumbnail/javaScript.jpg',
-  Reactjs: '/thumbnail/react.jpg',
+  React: '/thumbnail/react.jpg',
   etc: '/thumbnail/etc.jpg',
 };
