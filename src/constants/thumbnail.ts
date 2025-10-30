319e65d3cab405e7e7f4606adeb303ead087ed04
Dong Han <148463611+Dong-Han-Kim@users.noreply.github.com>
2025-10-30 21:45:00 +0900

    Fix/Nav 및 컨텐츠 스타일 개선 (#11)
    * WIP: save work
    
    * Fix: folder rename
    
    * Fix: Nav 및 컨텐츠 스타일 개선

--- src/constants/thumbnail.ts
+++ src/constants/thumbnail.ts
@@ -2,7 +2,7 @@ import { DefaultImage } from '../types/common';
 
 export const DEFAULT_THUMBNAIL: DefaultImage = {
   HTML: '/thumbnail/html.jpg',
-  CSS: '/thumbnail/CSS.jpg',
+  CSS: '/thumbnail/css.jpg',
   JavaScript: '/thumbnail/javaScript.jpg',
   Reactjs: '/thumbnail/react.jpg',
   etc: '/thumbnail/etc.jpg',
