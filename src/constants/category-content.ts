30d6ab9d1d49604cff6cf1032c8d5f219a8baa15
Dong Han <148463611+Dong-Han-Kim@users.noreply.github.com>
2025-10-30 22:53:19 +0900

    Fix/폴더명 변경 및 상수 변경 후 적용 (#12)
    * Fix: 폴더명 변경 및 상수 변경 후 적용
    
    * Delete src/posts/TypeScript directory
    
    * Delete src/posts/JavaScript directory
    
    * Delete src/posts/HTML directory
    
    * Delete src/posts/CSS directory
    
    * Delete src/posts/CS directory

---
+++ src/constants/category-content.ts
@@ -0,0 +1,36 @@
+import { CategoryContent } from '../types/common';
+
+export const CATEGORY_CONTENT: CategoryContent = {
+  html: {
+    name: 'HTML',
+    thumbnail: '/thumbnail/html.jpg',
+  },
+  css: {
+    name: 'CSS',
+    thumbnail: '/thumbnail/css.jpg',
+  },
+  javascript: {
+    name: 'JavaScript',
+    thumbnail: '/thumbnail/javaScript.jpg',
+  },
+  reactjs: {
+    name: 'React.js',
+    thumbnail: '/thumbnail/react.jpg',
+  },
+  nextjs: {
+    name: 'Next.js',
+    thumbnail: '/thumbnail/etc.jpg',
+  },
+  typescript: {
+    name: 'TypeScript',
+    thumbnail: '/thumbnail/etc.jpg',
+  },
+  projects: {
+    name: 'Project',
+    thumbnail: '/thumbnail/etc.jpg',
+  },
+  etc: {
+    name: 'Etc',
+    thumbnail: '/thumbnail/etc.jpg',
+  },
+};
