71ae42085dd9bd05a171ea5cbdedd21b47675678
Dong-Han-Kim <dh32110@gmail.com>
2025-11-30 20:29:37 +0900

    Feat: 댓글 생성 serverAction 구현

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,41 @@
-import { pgTable, serial, varchar, text, timestamp } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import {
+  pgTable,
+  serial,
+  varchar,
+  text,
+  timestamp,
+  integer,
+} from 'drizzle-orm/pg-core';
+
+export const posts = pgTable('posts', {
+  id: serial('id').primaryKey(),
+  slug: text('slug').notNull().unique(),
+  title: text('title').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
 
 export const comments = pgTable('comments', {
   id: serial('id').primaryKey(),
-  username: varchar('username', { length: 50 }).notNull(),
+  postId: integer('post_id')
+    .notNull()
+    .references(() => posts.id, { onDelete: 'cascade' }),
+  authorName: varchar('author_name', { length: 50 }).notNull(),
   passwordHash: text('password_hash').notNull(),
   content: text('content').notNull(),
+  parentId: integer('parent_id'),
   createdAt: timestamp('created_at').defaultNow().notNull(),
-  updateAt: timestamp('updated_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
+
+export const commentsRelations = relations(comments, ({ one, many }) => ({
+  parent: one(comments, {
+    fields: [comments.parentId],
+    references: [comments.id],
+    relationName: 'commentParent',
+  }),
+  replies: many(comments, {
+    relationName: 'commentReplies',
+  }),
+}));
